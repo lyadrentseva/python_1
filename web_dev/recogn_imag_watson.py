6e0906709de180976af94ef8dc857b873d765bc5
Lena <l.yadrentseva@gmail.com>
2018-05-17 09:26:46 +0300

    remove own key

--- web_dev/recogn_imag_watson.py
+++ web_dev/recogn_imag_watson.py
@@ -1,7 +1,7 @@
 from watson- import VisualRecognitionV3
 import json
 
-visual_recognition = VisualRecognitionV3(version='2018-03-19', api_key="892bbd779750cbbb1553f95682879c2d00401a2a")
+visual_recognition = VisualRecognitionV3(version='2018-03-19', api_key="**************")
 
 with open(r'path_to_image.jpg', 'rb') as images_file:
     car_results = visual_recognition.classify(images_file=images_file)
